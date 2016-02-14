2d23d6ce19d96aa31eb4e038023df3c0907224b9
An Tran <tranminhan.my@gmail.com>
2016-02-14 22:21:57 +0700

    check permission for task controller

--- app/controllers/task_controller.rb
+++ app/controllers/task_controller.rb
@@ -1,10 +1,26 @@
 class TaskController < ApplicationController
+	
+	before_action :authenticate_user!
+
   def show
   	project = Project.find(params[:project_id])
-  	@tasks = project.tasks.order(:tag)
-  	@task = Task.find(params[:id])
 
-  	@prev_task = @task.prev
-  	@next_task = @task.next
+  	joined = false
+
+  	if !current_user.nil? && !current_user.projects.nil?
+  		joined = current_user.projects.include?(project)
+  	end
+
+  	if joined
+	  	@tasks = project.tasks.order(:tag)
+	  	@task = Task.find(params[:id])
+
+	  	@prev_task = @task.prev
+	  	@next_task = @task.next
+	  else 
+	  	flash[:notice] = "Please join this course first!"
+	  	redirect_to project
+	  end 
+
   end
 end
