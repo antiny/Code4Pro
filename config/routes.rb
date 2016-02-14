259e83416425e4a1e8815993e917ba05adfde368
An Tran <tranminhan.my@gmail.com>
2016-02-14 21:09:59 +0700

    add subscriptions

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   ActiveAdmin.routes(self)
   devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
   get 'pages/about'
+  post '/free' => 'charge#free'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
