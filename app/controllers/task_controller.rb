59b91a8277d3f69ef84d79de761e28b1e7f40cbe
An Tran <tranminhan.my@gmail.com>
2016-02-14 18:00:29 +0700

    add wistia player

--- app/controllers/task_controller.rb
+++ app/controllers/task_controller.rb
@@ -3,5 +3,8 @@ class TaskController < ApplicationController
   	project = Project.find(params[:project_id])
   	@tasks = project.tasks.order(:tag)
   	@task = Task.find(params[:id])
+
+  	@prev_task = @task.prev
+  	@next_task = @task.next
   end
 end
