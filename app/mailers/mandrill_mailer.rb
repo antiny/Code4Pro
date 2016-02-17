5096f2958a0edf2b4188ddcd58297741bb01db8a
An Tran <tranminhan.my@gmail.com>
2016-02-17 15:47:58 +0700

    receipt email

--- app/mailers/mandrill_mailer.rb
+++ app/mailers/mandrill_mailer.rb
@@ -23,4 +23,24 @@ class MandrillMailer < ApplicationMailer
 		mandrill_client.messages.send_template template_name, template_content, message
 	end 
 
+	def new_receipt(user, project)
+		template_name = "new-receipt"
+		template_content = []
+		message = {
+			to: [{email: "#{user.email}"}],
+			subject: "Code4Pro - Your Receipt",
+			merge_vars: [
+				{
+					rcpt: user.email,
+					vars: [{ name: "STUDENT_NAME", content: user.name },
+					       { name: "PROJECT_NAME", content: project.name },		
+					       { name: "PROJECT_PRICE", content: project.price }]			
+				}
+			]
+		}
+
+		byebug
+		mandrill_client.messages.send_template template_name, template_content, message
+	end
+
 end
--- app/models/subscription.rb
+++ app/models/subscription.rb
@@ -1,4 +1,15 @@
 class Subscription < ActiveRecord::Base
   belongs_to :user
   belongs_to :project
+
+  after_create :send_receipt_email
+
+  private 
+  	def send_receipt_email
+  		@user = User.find_by_email(self.user.email)
+  		@project = Project.find(self.project_id)
+
+  		byebug
+  		MandrillMailer.new_receipt(@user, @project).deliver_now	
+  	end 
 end
